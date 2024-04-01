59300bf20a672d8420ee168c8bc91cd0d413c004
RoLO0u <roman.fedunn@gmail.com>
2024-04-01 22:51:52 +0300

    Fixing creating user SQL bug

--- templates/database/sql_queries/create/user.sql
+++ templates/database/sql_queries/create/user.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.users
     (userid, packs, username, language, name, title, emoji, stickers, emojis, sticker)
 	VALUES
-    (%s, ARRAY []::VARCHAR [], %s, 'en', NULL, NULL, NULL, ARRAY []::VARCHAR [], ARRAY []::VARCHAR []);
+    (%s, ARRAY []::VARCHAR [], %s, 'en', NULL, NULL, NULL, ARRAY []::VARCHAR [], ARRAY []::VARCHAR [], NULL);
