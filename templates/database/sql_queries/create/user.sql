24e19f7ae3e6c2710887d96c4f3cf9650022a2fa
RoLO0u <roman.fedunn@gmail.com>
2025-03-20 01:27:38 +0200

    syntaxerror sql

--- templates/database/sql_queries/create/user.sql
+++ templates/database/sql_queries/create/user.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.users
     (userid, packs, username, language, name, title, emoji, stickers, emojis, sticker, image)
 	VALUES
-    (%s, ARRAY []::VARCHAR [], %s, 'en', NULL, NULL, NULL, ARRAY []::VARCHAR [], ARRAY []::VARCHAR [], NULL, VARCHAR []);
+    (%s, ARRAY []::VARCHAR [], %s, 'en', NULL, NULL, NULL, ARRAY []::VARCHAR [], ARRAY []::VARCHAR [], NULL, NULL);
