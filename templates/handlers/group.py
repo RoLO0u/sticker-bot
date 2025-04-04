e2156e08b5c4183280d63c3a996c3530ef8157bc
RoLO0u <roman.fedunn@gmail.com>
2025-04-05 00:41:38 +0300

    Warn people that groups are not available yet

--- templates/handlers/group.py
+++ templates/handlers/group.py
@@ -1,21 +1,8 @@
-from typing import Type
 from aiogram import Router, types, F
-
-from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-
-from templates.database import baseDB
-from templates.FSM_groups import StartFSM, JoiningFSM, ManagingFSM
-from templates.markups import start_button, managing_button_2
-from templates.types import Answers, texts, texts_buttons
 
 router = Router()
 
 @router.message(F.chat.type.in_({"group", "supergroup"}), F.text)
-async def group_start( \
-        message: types.Message, \
-        state: FSMContext, \
-        user: baseDB.User \
-        ) -> None:
+async def group_warning(message: types.Message) -> None:
     
-    await message.answer("Hello")
+    await message.answer("This bot is not available in groups yet. Please, use it in private chat.")
